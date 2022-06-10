b70dd52e2b694717de940d83359f63db3cfb9c8d
Daniel Riverol <dannyriverol@gmail.com>
2022-06-09 22:51:58 -0300

    Primer commit

---
+++ clase_1/js/main.js
@@ -0,0 +1,44 @@
+//declaracion
+let nombre;
+let apellido;
+let Nombre;
+let nombreCompleto;
+
+//asiganacion/incializando
+//nombre = "Juan";
+apellido = "Perez";
+Nombre = "Maria";
+nombreCompleto = nombre + " " + apellido;
+
+let numero1 = "1";
+let numero2 = 23;
+let resultado = numero1 + numero2;
+
+//console.log(resultado);
+
+const PI = 3.141516;
+
+//nombre = 09876543;
+apellido = "Gonzalez";
+
+//let nombreIngresado = prompt("Ingresa tu nombre");
+//let apellidoIngresado = prompt("Ingresa tu apellido");
+
+/* console.log("Mensaje");
+console.log(nombre);
+console.log(nombreIngresado + " " + apellidoIngresado);
+console.log("El resultado de la suma es: "+ resultado);
+console.log(12 * 4); */
+
+//console.log(nombre);
+/* alert("Mensaje")
+alert(nombreCompleto) */
+//parseInt()
+//parseFloat()
+let num1 = parseFloat(prompt("Ingresa un numero"));
+let num2 = prompt("Ingresa otro numero");
+num2 = parseFloat(num2);
+
+let suma = num1 + num2;
+
+console.log(suma;);
